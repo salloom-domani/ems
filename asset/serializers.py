451bfad66a430fa33e773694f406e971ed8f59dc
Salem Domani <salem.domani@gmail.com>
2022-05-28 20:58:55 +0300

    Fix referenced modules.

--- asset/serializers.py
+++ asset/serializers.py
@@ -1,6 +1,5 @@
-
 from rest_framework.serializers import ModelSerializer
-from models import Asset, Department, Employee
+from .models import Asset, Department, Employee
 
 class AssetSerializer(ModelSerializer):
     class Meta:
--- asset/urls.py
+++ asset/urls.py
@@ -1,5 +1,5 @@
 from rest_framework.routers import DefaultRouter
-from views import AssetViewSet, DepartmentViewSet, EmployeeViewSet
+from .views import AssetViewSet, DepartmentViewSet, EmployeeViewSet
 
 router = DefaultRouter()
 router.register(r'assets', AssetViewSet)
