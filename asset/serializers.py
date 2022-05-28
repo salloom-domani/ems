bf9ed6a24324dc0fb56c3a9b1087960575339853
Salem Domani <salem.domani@gmail.com>
2022-05-28 20:47:49 +0300

    Add basic views and serializers for asset app.

---
+++ asset/serializers.py
@@ -0,0 +1,18 @@
+
+from rest_framework.serializers import ModelSerializer
+from models import Asset, Department, Employee
+
+class AssetSerializer(ModelSerializer):
+    class Meta:
+        model = Asset
+        fields = '__all__'
+
+class DepartmentSerializer(ModelSerializer):
+    class Meta:
+        model = Department
+        fields = '__all__'
+
+class EmployeeSerializer(ModelSerializer):
+    class Meta:
+        model = Employee
+        fields = '__all__'
--- asset/views.py
+++ asset/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from . import models
+from . import serializers
 
-# Create your views here.
+
+class AssetViewSet(ModelViewSet):
+    serializer_class = serializers.AssetSerializer
+    queryset = models.Asset.objects.all()
+
+class DepartmentViewSet(ModelViewSet):
+    serializer_class = serializers.DepartmentSerializer
+    queryset = models.Department.objects.all()
+
+class EmployeeViewSet(ModelViewSet):
+    serializer_class = serializers.EmployeeSerializer
+    queryset = models.Employee.objects.all()
