8a84eac57bf65c24a2590d53dae47a3a18664ea2
Salem Domani <salem.domani@gmail.com>
2022-05-28 20:56:38 +0300

    Add basic urls for asset app.

---
+++ asset/urls.py
@@ -0,0 +1,9 @@
+from rest_framework.routers import DefaultRouter
+from views import AssetViewSet, DepartmentViewSet, EmployeeViewSet
+
+router = DefaultRouter()
+router.register(r'assets', AssetViewSet)
+router.register(r'departments', DepartmentViewSet)
+router.register(r'employees', EmployeeViewSet)
+
+urlpatterns = router.urls
