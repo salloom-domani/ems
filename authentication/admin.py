4e2bc3673de52fe39047f166de9383d567ceb9bf
Salem Domani <salem.domani@gmail.com>
2022-05-28 17:02:25 +0300

    Initial commit.

---
+++ authentication/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from . import models
+
+
+@admin.register(models.User)
+class UserAdmin(BaseUserAdmin):
+    pass
+
+@admin.register(models.Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    pass
---
+++ authentication/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    pass
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    picture = models.ImageField(upload_to='profile/images')
+
