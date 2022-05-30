5752097ef7eea2b4eea22ca38cdc027796b1082f
Salem Domani <salem.domani@gmail.com>
2022-05-31 00:58:45 +0300

    Implement admin pages.

--- asset/admin.py
+++ asset/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Asset)
+class AssetAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    pass
--- request/admin.py
+++ request/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Request)
+class RequestAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.FixRequest)
+class FixRequestAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.TransferRequest)
+class TransferRequestAdmin(admin.ModelAdmin):
+    pass
