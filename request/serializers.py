649d7678865be610981e75607208af2fd3918e79
Salem Domani <salem.domani@gmail.com>
2022-07-22 18:36:48 +0300

    Customize request app.

--- request/serializers.py
+++ request/serializers.py
@@ -1,4 +1,6 @@
+from email.policy import default
 from rest_framework.serializers import ModelSerializer
+from rest_framework import fields
 from .models import Request, FixRequest, TransferRequest
 
 class RequestSerializer(ModelSerializer):
@@ -6,12 +8,19 @@ class RequestSerializer(ModelSerializer):
         model = Request
         fields = '__all__'
 
+class ApproveRequestSerializer(ModelSerializer):
+    class Meta:
+        model = Request
+        fields = ['approve_number']
+
 class FixRequestSerializer(ModelSerializer):
+    type = fields.CharField(read_only=True, default='fixrequest')
     class Meta:
         model = FixRequest
         fields = '__all__'
 
 class TransferRequestSerializer(ModelSerializer):
+    type = fields.CharField(read_only=True, default='transferrequest')
     class Meta:
         model = TransferRequest
         fields = '__all__'
--- request/views.py
+++ request/views.py
@@ -1,26 +1,48 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import GenericViewSet, ReadOnlyModelViewSet
+from rest_framework import mixins
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from . import models
 from . import serializers
 
 
-class RequestViewSet(ModelViewSet):
+class RequestViewSet(
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
     serializer_class = serializers.RequestSerializer
     queryset = models.Request.objects.all()
 
-    @action(detail=True, methods=['get'])
+    @action(detail=True, methods=["get"])
     def approve(self, request, pk=None):
         request_object = self.get_object()
         request_object.approve(1)
         request_object.save()
-        return Response({ 'status': 'Approved successfully!'})
-        
+        return Response({"status": "Approved successfully!"})
 
-class FixRequestViewSet(ModelViewSet):
+    def get_serializer_class(self):
+        if self.request.method == "PUT":
+            return serializers.ApproveRequestSerializer
+        return serializers.RequestSerializer
+
+
+class FixRequestViewSet(
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
     serializer_class = serializers.FixRequestSerializer
     queryset = models.FixRequest.objects.all()
 
-class TransferRequestViewSet(ModelViewSet):
+
+class TransferRequestViewSet(
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
     serializer_class = serializers.TransferRequestSerializer
     queryset = models.TransferRequest.objects.all()
