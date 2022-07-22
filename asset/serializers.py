d52747652b6e4aa8e5b69350a160d384901d39b2
Salem Domani <salem.domani@gmail.com>
2022-07-22 22:28:33 +0300

    Customize asset app.

--- asset/serializers.py
+++ asset/serializers.py
@@ -6,12 +6,24 @@ class AssetSerializer(ModelSerializer):
         model = Asset
         fields = '__all__'
 
+class SimpleAssetSerializer(ModelSerializer):
+    class Meta:
+        model = Asset
+        fields = ['id', 'name']
+
 class DepartmentSerializer(ModelSerializer):
     class Meta:
         model = Department
         fields = '__all__'
 
+class SimpleDepartmentSerializer(ModelSerializer):
+    class Meta:
+        model = Department
+        fields = ['id', 'name']
+
 class EmployeeSerializer(ModelSerializer):
+    my_departments = SimpleDepartmentSerializer(many=True, read_only=True)
+    assets = SimpleAssetSerializer(many=True, read_only=True)
     class Meta:
         model = Employee
         fields = '__all__'
