e810092069243e7192e61422c6e3eb0e78c23ee1
Salem Domani <salem.domani@gmail.com>
2022-07-22 13:39:09 +0300

    Add relation between Asset model and Employee model.

--- asset/models.py
+++ asset/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 from django.conf import settings
 
+
 class Department(models.Model):
     name = models.CharField(max_length=255)
-    admin = models.ForeignKey('Employee', on_delete=models.SET_NULL, null=True, blank=True, related_name='my_departments')
+    admin = models.ForeignKey(
+        "Employee",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="my_departments",
+    )
 
 
 class Employee(models.Model):
@@ -12,21 +19,25 @@ class Employee(models.Model):
     is_admin = models.BooleanField()
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
+
 class Asset(models.Model):
-    FINE = 'F'
-    BROKEN = 'B'
-    IN_MAINTENANCE = 'M'
+    FINE = "F"
+    BROKEN = "B"
+    IN_MAINTENANCE = "M"
     STATUS_CHOICES = [
-        (FINE, 'Fine'),
-        (BROKEN, 'Broken'),
-        (IN_MAINTENANCE, 'In maintenance'),
+        (FINE, "Fine"),
+        (BROKEN, "Broken"),
+        (IN_MAINTENANCE, "In maintenance"),
     ]
 
     name = models.CharField(max_length=255)
     model = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
-    image = models.ImageField(upload_to='assets/')
+    image = models.ImageField(upload_to="assets/")
     price = models.PositiveSmallIntegerField()
     status = models.CharField(max_length=2, choices=STATUS_CHOICES, default=FINE)
     brand = models.CharField(max_length=255)
     category = models.CharField(max_length=255)
+    reporter = models.ForeignKey(
+        Employee, on_delete=models.CASCADE, related_name="assets"
+    )
